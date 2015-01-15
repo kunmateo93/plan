20d7000799690250610033556c78ffca5f68b7ff
kunmateo93 <wozniak.mateusz@interia.eu>
2015-01-15 23:18:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 //provide a sensible default for local development
+db_name = "nodejs"
 mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
